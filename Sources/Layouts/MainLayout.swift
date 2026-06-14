485c8735896035a7299553b5c766fe056adaafba
Enzo Ferroni <enzoferroni@hotmail.com>
2026-06-14 20:09:54 -0300

    refactor: merge stylesheets into single styles.css

--- Sources/Layouts/MainLayout.swift
+++ Sources/Layouts/MainLayout.swift
@@ -11,11 +11,7 @@ struct MainLayout: Layout {
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="css/base.css">
-            <link rel="stylesheet" href="css/hero.css">
-            <link rel="stylesheet" href="css/features.css">
-            <link rel="stylesheet" href="css/faq.css">
-            <link rel="stylesheet" href="css/footer.css">
+            <link rel="stylesheet" href="css/styles.css">
             """
             content
             #"<script src="js/app.js" defer></script>"#
